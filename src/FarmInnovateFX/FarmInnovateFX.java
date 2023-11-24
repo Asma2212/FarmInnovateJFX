e2c189b4eaedbfba7010a7ef01a26b9bc927110b
abirbelh <117998646+abirbelh@users.noreply.github.com>
2023-11-24 19:14:24 +0100

    fdfqsfs

--- src/FarmInnovateFX/FarmInnovateFX.java
+++ src/FarmInnovateFX/FarmInnovateFX.java
@@ -21,6 +21,7 @@ public class FarmInnovateFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
+            System.out.println("hellooooo");
         Parent root = FXMLLoader.load(getClass().getResource("/View/Inventaire/Batiments.fxml"));
         Scene scene = new Scene(root);
      
